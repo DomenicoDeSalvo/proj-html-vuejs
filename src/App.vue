871a7bbf00f031b609a12f6c46f0745302934a84
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-23 00:36:56 +0200

    Added scaffolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+  export default {
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  }
+</script>
+
+<style lang="scss">
+  @use './assets/style/general';
+</style>
