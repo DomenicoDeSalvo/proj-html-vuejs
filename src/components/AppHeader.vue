e0897215e87375234c1aa0e2b61feefb5795b1a0
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-23 12:16:46 +0200

    Added temporary header, added font awesome, changed files dispositions

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <nav class="container_85">
+            <div class="row">
+                <ul class="mt-3 list-unstyled d-flex gap-3 justify-content-end">
+                    <li v-for="(nav, i) in navBar" :key="i">{{ nav.text }}</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                navBar:[
+                    {
+                        text:'Home',
+                        href:'#'
+                    },
+                    {
+                        text:'About Me',
+                        href:'#'
+                    },
+                    {
+                        text:'Testimonials',
+                        href:'#'
+                    },
+                    {
+                        text:'My Blog',
+                        href:'#'
+                    },
+                    {
+                        text:'Meetups',
+                        href:'#'
+                    },
+                    {
+                        text:'Shop',
+                        href:'#'
+                    },
+                    {
+                        text:'Contact Me',
+                        href:'#'
+                    },
+                    {
+                        text:'C',
+                        href:'#'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+//BOOTSTRAP
 import "bootstrap/dist/css/bootstrap.css";
 
-createApp(App).mount('#app')
+//FONT AWESOME
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {faCartShopping as cart} from '@fortawesome/free-solid-svg-icons'
+
+library.add(cart)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js";
