3ceca5888bd3e1ae6ba1c8b7689d79299048c819
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-23 23:50:19 +0200

    Added "WhereFind" section, where the platform where the author's book is available are displayed.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,20 @@
 <template>
     <AppHero/>
     <SectionAbout/>
+    <SectionWhereFind/>
 </template>
 
 <script>
     import AppHero from './main_components/AppHero.vue';
     import SectionAbout from './main_components/SectionAbout.vue';
+    import SectionWhereFind from './main_components/SectionWhereFind.vue';
+
 
     export default {
         components:{
             AppHero,
             SectionAbout,
+            SectionWhereFind
         }
         
     }
--- src/components/main_components/SectionWhereFind.vue
+++ src/components/main_components/SectionWhereFind.vue
@@ -1,1 +1,36 @@
-w   
+<template>
+    <section class="where_find d-flex align-items-center">
+        <div class="container_55">
+            <div class="row row-columns-4 align-items-center">
+                <div class="col">
+                    <img src="/public/ebay-logo.png" alt="">
+                </div>
+                <div class="col">
+                    <img src="/public/audible-logo.png" alt="">
+                </div>
+                <div class="col">
+                    <img src="/public/bb-logo.png" alt="">
+                </div>
+                <div class="col">
+                    <img src="/public/kindlefire-logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use'../../assets/Style/partials/variables' as*;
+
+    .where_find{
+        background-color: $brand_beige;
+        height: 120px;
+    }
+
+</style>
